8536ce8576ea79492464e78927df1f31206b12ff
noamm19-meet <noamm19@meet.mit.edu>
2017-07-23 19:02:36 +0300

    Stopping here

---
+++ MeetinTurtle.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.mainloop()
+
